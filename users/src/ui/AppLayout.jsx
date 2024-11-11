7922fed3a8d66213825811e404d9b2777316459a
Van Dong <vandong78py@gmail.com>
2024-11-11 20:52:27 +0700

    Footer

--- users/src/ui/AppLayout.jsx
+++ users/src/ui/AppLayout.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Headers from "../components/header/Headers";
 import { Outlet } from "react-router-dom";
 import HeaderBottom from "../components/header/HeaderBottom";
+import Footer from "../components/footer/Footer";
 
 function AppLayout() {
   return (
@@ -11,6 +12,7 @@ function AppLayout() {
       <main>
         <Outlet />
       </main>
+      <Footer />
     </>
   );
 }
