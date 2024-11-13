76aa6153ba32abe7645b3f5789ff29c76931f877
Lequocluan <quocluan15102003@gmail.com>
2024-11-13 11:03:01 +0700

    tao MainNav Sidebar

---
+++ admin/src/ui/MainNav.jsx
@@ -0,0 +1,10 @@
+function MainNav(){
+    return(
+        <nav>
+            <ul>
+                <li></li>
+            </ul>
+        </nav>
+    )
+}
+export default MainNav;
---
+++ admin/src/ui/Sidebar.jsx
@@ -0,0 +1,13 @@
+import Logo from "./Logo";
+import MainNav from "./MainNav";
+
+
+function Sidebar(){
+    return(
+        <aside>
+            <Logo/>
+            <MainNav/>
+        </aside>
+    )
+}
+export default Sidebar
