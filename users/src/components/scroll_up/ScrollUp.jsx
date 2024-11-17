71ee625580f430842b92be5d25a8bc8f3699d0bc
Van Dong <vandong78py@gmail.com>
2024-11-18 00:24:15 +0700

    Scroll Up

---
+++ users/src/components/scroll_up/ScrollUp.jsx
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+import { AiOutlineArrowUp } from "react-icons/ai";
+import "./scrollup.css";
+
+const ScrollUp = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 300) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  return (
+    isVisible && (
+      <button
+        className="scroll-up-btn"
+        onClick={scrollToTop}
+        aria-label="Scroll to top"
+      >
+        <AiOutlineArrowUp />
+      </button>
+    )
+  );
+};
+
+export default ScrollUp;
