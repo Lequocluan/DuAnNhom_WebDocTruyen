9fb637e70e8a8b84d1ba0e69a8320538b938bdaf
Tiến <letien3413@gmail.com>
2024-11-22 16:55:36 +0700

    feat: search story

--- users/src/components/header/FormSearch.jsx
+++ users/src/components/header/FormSearch.jsx
@@ -1,33 +1,126 @@
-import React from "react";
-const handleInputChange = (e) => {
-  setSearchKeyword(e.target.value);
-};
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 function FormSearch() {
+  const [keyword, setKeyword] = useState("");
+  const [results, setResults] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isFocused, setIsFocused] = useState(false);
+
+  // Khởi tạo hook useNavigate
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setIsLoading(true);
+    const timeoutId = setTimeout(() => {
+      if (keyword.trim() !== "") {
+        fetchResults();
+      }
+    }, 500);
+
+    return () => clearTimeout(timeoutId);
+  }, [keyword]);
+
+  const fetchResults = async () => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(
+        `https://truyen.ntu264.vpsttt.vn/api/search?keyword=${encodeURIComponent(
+          keyword
+        )}`
+      );
+      const data = await response.json();
+      setResults(data.body.data);
+    } catch (error) {
+      console.error("Error fetching search results:", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const handleInputChange = (e) => {
+    setKeyword(e.target.value);
+  };
+
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    if (keyword.trim()) {
+      navigate(`/search?keyword=${encodeURIComponent(keyword)}`);
+    }
+  };
+
   return (
     <>
-      <form className="d-flex header__form-search">
+      <form
+        className="d-flex header__form-search"
+        onSubmit={handleSearchSubmit}
+        autoComplete="off"
+      >
         <input
+          required
           className="form-control search-story"
           type="text"
-          placeholder="Tìm kiếm"
-          name="key_word"
-          value=""
+          placeholder="Tìm kiếm truyện theo tên hoặc tác giả"
+          name="keyword"
+          value={keyword}
           onChange={handleInputChange}
+          onFocus={() => setIsFocused(true)}
+          onBlur={() => setTimeout(() => setIsFocused(false), 200)}
         />
-        <div className="col-12 search-result shadow no-result d-none">
-          <div className="card text-white bg-light">
-            <div className="card-body p-0">
-              <ul className="list-group list-group-flush d-none">
-                <li className="list-group-item">
-                  <a href="#" className="text-dark hover-title">
-                    Tự cẩm
-                  </a>
-                </li>
-              </ul>
-            </div>
+        {isFocused && keyword.trim() !== "" && (
+          <div className="col-12 search-result shadow">
+            {isLoading ? (
+              <p className="bg-white text-[#343a40] p-10 text-center">
+                Đang tìm kiếm...
+              </p>
+            ) : keyword.trim() !== "" && results.length === 0 ? (
+              <p className="bg-white text-[#343a40] p-10 text-center">
+                Không tìm thấy kết quả nào
+              </p>
+            ) : (
+              <div className="card text-white bg-light">
+                <div className="card-body p-0">
+                  <ul className="list-group list-group-flush">
+                    {results.length > 0 ? (
+                      results.map((item, index) => (
+                        <Link
+                          to={`/${item.slug}`}
+                          onClick={() => setKeyword("")}
+                          key={index}
+                        >
+                          <li className="list-group-item d-flex align-items-center">
+                            <img
+                              src={item.story_picture.path}
+                              alt={item.story_picture.title}
+                              className="rounded me-3"
+                              style={{
+                                width: "50px",
+                                height: "50px",
+                                objectFit: "cover",
+                              }}
+                            />
+                            <div>
+                              <a className="text-dark hover-title fw-bold">
+                                {item.name}
+                              </a>
+                              <p className="text-muted mb-0 fst-italic">
+                                {item.author.full_name}
+                              </p>
+                            </div>
+                          </li>
+                        </Link>
+                      ))
+                    ) : (
+                      <li className="list-group-item">
+                        Không tìm thấy kết quả nào
+                      </li>
+                    )}
+                  </ul>
+                </div>
+              </div>
+            )}
           </div>
-        </div>
+        )}
 
         <button className="btn" type="submit">
           <svg
