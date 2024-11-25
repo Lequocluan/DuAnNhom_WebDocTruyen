93a45a384963a38dea06249bf06b72dc96b3cb62
Van Dong <vandong78py@gmail.com>
2024-11-25 11:13:28 +0700

    Fix Story Done

--- users/src/components/section_index/SectionStoriesDoneItem.jsx
+++ users/src/components/section_index/SectionStoriesDoneItem.jsx
@@ -1,42 +1,38 @@
 import { Link } from "react-router-dom";
 import img2 from "../../assets/images/ngao_the_dan_than.jpg";
 
-function SectionStoriesDoneItem({ categories, story_picture, name }) {
+function SectionStoriesDoneItem({ categories, story_picture, name, slug }) {
   const imagePath = story_picture?.path || img2;
 
   return (
     <>
-      {categories.map((category) => {
-        return (
-          <div className="story-item-full text-center" key={category.id}>
-            <Link
-              // to={`/${category.slug}/${chapter.slug}`}
-              to={`/category/${category.slug}`}
-              className="d-block story-item-full__image"
-            >
-              <img
-                src={imagePath}
-                alt=""
-                className="img-fluid w-100"
-                width="150"
-                height="230"
-                loading="lazy"
-              />
-            </Link>
-            <h3 className="fs-6 story-item-full__name fw-bold text-center mb-0">
-              <Link
-                to={`/category/${category.slug}`}
-                className="text-decoration-none text-one-row story-name"
-              >
-                {name}
-              </Link>
-            </h3>
-            <span className="story-item-full__badge badge text-bg-success">
-              {"Full"}
-            </span>
-          </div>
-        );
-      })}
+      <div className="story-item-full text-center">
+        <Link
+          // to={`/${category.slug}/${chapter.slug}`}
+          to={`/truyen-chu/${slug}`}
+          className="d-block story-item-full__image"
+        >
+          <img
+            src={imagePath}
+            alt=""
+            className="img-fluid w-100"
+            width="150"
+            height="230"
+            loading="lazy"
+          />
+        </Link>
+        <h3 className="fs-6 story-item-full__name fw-bold text-center mb-0">
+          <Link
+            to={`/category/${slug}`}
+            className="text-decoration-none text-one-row story-name"
+          >
+            {name}
+          </Link>
+        </h3>
+        <span className="story-item-full__badge badge text-bg-success">
+          {"Full"}
+        </span>
+      </div>
     </>
   );
 }
