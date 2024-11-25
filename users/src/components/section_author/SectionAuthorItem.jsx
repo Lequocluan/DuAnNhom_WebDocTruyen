4a23cb9bcd8522496dd73f4accbe9f8f74f6bcc8
Van Dong <vandong78py@gmail.com>
2024-11-25 19:48:12 +0700

    Category Author

---
+++ users/src/components/section_author/SectionAuthorItem.jsx
@@ -0,0 +1,62 @@
+import { Link } from "react-router-dom";
+import img11 from "../../assets/images/diu_dang_tan_xuong.jpg";
+
+function SectionAuthorItem({ novel, novel_type }) {
+  // console.log(novel)
+  const novelConfig = {
+    comic: {
+      type: "Truyện tranh",
+      name: novel.name,
+      status: novel.status,
+      thumbnail: novel.thumbnail,
+      slug: `/truyen-tranh/${novel.slug}`,
+    },
+    story: {
+      type: "Truyện chữ",
+      name: novel.name,
+      status: novel.status,
+      thumbnail: novel.story_picture?.path ?? img11,
+      slug: `/truyen-chu/${novel.slug}`,
+    },
+  };
+  const novelInfo = novelConfig[novel_type];
+  return (
+    <>
+      <div className="story-item">
+        <Link to={`${novelInfo.slug}`} className="d-block text-decoration-none">
+          <div className="story-item__image">
+            <img
+              src={novelInfo.thumbnail}
+              alt={novelInfo.name}
+              className="img-fluid"
+              style={{ width: "150px", height: "230px" }}
+              loading="lazy"
+            />
+          </div>
+          <h3 className="story-item__name text-one-row story-name truncate">
+            {novelInfo.name}
+          </h3>
+
+          <div className="list-badge">
+            {novel_type == "comic" ? (
+              <span className="story-item__badge story-item__badge-hot badge text-bg-primary">
+                {novelInfo.type}
+              </span>
+            ) : (
+              <span className="story-item__badge story-item__badge-hot badge text-bg-warning">
+                {novelInfo.type}
+              </span>
+            )}
+            {novelInfo.status === 2 && (
+              <span className="story-item__badge badge text-bg-success">
+                Full
+              </span>
+            )}
+          </div>
+        </Link>
+      </div>
+    </>
+  );
+}
+
+export default SectionAuthorItem;
--- users/src/ui/Category.jsx
+++ users/src/ui/Category.jsx
@@ -1,5 +1,3 @@
-import { useEffect } from "react";
-import { useParams } from "react-router-dom";
 import SectionCategory from "../components/section_category/SectionCategory";
 
 function Category() {
